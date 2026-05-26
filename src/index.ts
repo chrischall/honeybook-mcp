2c86c7d41ae3dd3ddc59ad38c3c0dce2fcda080a
chrischall <chris.c.hall@gmail.com>
2026-05-26 13:08:13 -0400

    chore(main): release 0.2.4 (#42)
    :robot: I have created a release *beep* *boop*
    ---
    
    
    ##
    [0.2.4](https://github.com/chrischall/honeybook-mcp/compare/v0.2.3...v0.2.4)
    (2026-05-26)
    
    
    ### Bug Fixes
    
    * **ci:** substitute repo name in publish workflow
    ([#41](https://github.com/chrischall/honeybook-mcp/issues/41))
    ([8a73398](https://github.com/chrischall/honeybook-mcp/commit/8a73398bf6e8ce1cd47637428c18b3895a249766))
    
    ---
    This PR was generated with [Release
    Please](https://github.com/googleapis/release-please). See
    [documentation](https://github.com/googleapis/release-please#release-please).

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ import { registerInvoiceTools } from './tools/invoices.js';
 
 const server = new McpServer({
   name: 'honeybook-mcp',
-  version: '0.2.3', // x-release-please-version
+  version: '0.2.4', // x-release-please-version
 });
 
 registerSessionTools(server);
